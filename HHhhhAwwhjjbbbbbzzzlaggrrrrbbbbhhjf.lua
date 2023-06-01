96b9edcd0dd50804fa6af994e5532c22e0668cdc
Tanshu334 <94998344+Tanshu334@users.noreply.github.com>
2023-06-01 17:58:08 +0200

    Create HHhhhAwwhjjbbbbbzzzlaggrrrrbbbbhhjf.lua

---
+++ HHhhhAwwhjjbbbbbzzzlaggrrrrbbbbhhjf.lua
@@ -0,0 +1,378 @@
+{
+  "players": {
+    "384718982625886208": {
+      "hash": "1596c3e5c9bd2cc1ef62d2456b02159934f2048abcd5c197107bc23c022653ddc6947588d59a0e7eb202d9de990854c9fad52f81341e77d35ec846219a369029",
+      "attackable": false,
+      "notag": true
+    },
+    "837815287897128990": {
+      "hash": "e0ff711d19912266a468bddc53a186a2b529f399021dfc3c90d68dafc759af11717bf6910dff7da6f9aeda717253167e735f180bf6af76389991f64414889248",
+      "attackable": false,
+      "notag": false
+    },
+    "548146409065545730": {
+      "hash": "c067e91dec67df9b95b9ebac22b6a5fef5b6d790b80a41430a9f5ccf3e6a233cacae03eafcad0e5d57fb85a2b083cbfc7cc1c073a4c87b03f53ac206b5bd2ec3",
+      "attackable": false,
+      "notag": false
+    },
+    "709094619161296939": {
+      "hash": "0d5ed58b4a9d72a73e3aac0dd508ee9ff172e92bf796891b951aac7dac57e5db34c1cb75a20e24ff1dbfc4d60eec6badfddc7be94a640552ce1c107c3b0b663b",
+      "attackable": false,
+      "notag": false
+    }
+  },
+  "owners": {
+    "1059269872011444354": {
+      "hash": "0422548c65b54387dee64524ef4d45330c1826abd267e190b655fdb05997198bc219c56c77092aff81d89ad88596761e60762581cb0b1e1ce8f468973822d877",
+      "attackable": false,
+      "notag": false
+    }
+  },
+  "chattags": {
+    "1596c3e5c9bd2cc1ef62d2456b02159934f2048abcd5c197107bc23c022653ddc6947588d59a0e7eb202d9de990854c9fad52f81341e77d35ec846219a369029": {
+      "NameColor": {
+        "r": 3,
+        "g": 207,
+        "b": 252
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 3,
+            "g": 207,
+            "b": 252
+          },
+          "TagText": "PRISM OWNER"
+        }
+      ]
+    },
+    "04a06c7cdf52342dc1cc337cc9c9ea51aecfeea6e9f793547bbfc0eb128e419ef55e09f88c103b75740072f4bc64ef84b9f2061b71b6dfd4f0d65a20f85cfa93": {
+      "NameColor": {
+        "r": 255,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 255,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "VAPE USER"
+        }
+      ]
+    },
+    "6864c98b019f08143db0a2b22840d5996d14acf6e67296b91c507ec674d227f973ec25010be777c5122774d4b5295cf9057cabcc2f1450d7f16e6b5b03111e2d": {
+      "NameColor": {
+        "r": 255,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 255,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "VAPE USER"
+        }
+      ]
+    },
+    "8e4c5d0749162723901cb682b566b576f217651de17d3a7da29645ff30292b6b449f6f89843b643b8c40efc1a2989b65daac48f43039f32e8fdf2485649167be": {
+      "NameColor": {
+        "r": 0,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 0,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "TYPEWRITER"
+        }
+      ]
+    },
+    "05795c6d051397fdc5701410e55fa79191988a5015d5beb50d9465845e6cd1c5d16949682036bbb0e51301adf227e07d7862f4da2a1223d63a1bc8a506bf2223": {
+      "NameColor": {
+        "r": 255,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 255,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "VAPE USER"
+        }
+      ]
+    },
+    "dbb30e63cda6e7313437b91185ec928399a18f2454553243769ab6515c22ec6d48f4cd69741f4f01b3c4cee9177fbfd8487eb3d123368bfe7ceeee0f74a94cb1": {
+      "NameColor": {
+        "r": 255,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 255,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "VAPE USER"
+        }
+      ]
+    },
+    "033c757276ceb582a657e96f29a2acad8d19d3d35c2a7631b70d43d1b1fe93401cbf8921911601db5167a4588af90c664c21d798ca164956efa29c5be187273b": {
+      "NameColor": {
+        "r": 255,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 255,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "VAPE USER"
+        }
+      ]
+    },
+    "cfe039e7f9c782f2a46b9771b21cc8a8a4b370326dc78965b357b269b85ae66ac1bc3621d435b9f030e4f5bdd326b91cd88fa555cce829b1170638c5350048f9": {
+      "NameColor": {
+        "r": 174,
+        "g": 255,
+        "b": 53
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 174,
+            "g": 255,
+            "b": 53
+          },
+          "TagText": "BALLER"
+        }
+      ]
+    },
+    "21df861cf87c4bfc9ba7de6009a0907c66a845a93ff7063c9e6c03cb823477c71c843fa10f88de423f33a23c4c5de4c60fbba771c1c9c8af9545cd665e0d2c8a": {
+      "NameColor": {
+        "r": 255,
+        "g": 150,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 255,
+            "g": 150,
+            "b": 0
+          },
+          "TagText": "CONSTRUCTION WORKER"
+        }
+      ]
+    },
+    "0d5ed58b4a9d72a73e3aac0dd508ee9ff172e92bf796891b951aac7dac57e5db34c1cb75a20e24ff1dbfc4d60eec6badfddc7be94a640552ce1c107c3b0b663b": {
+      "NameColor": {
+        "r": 174,
+        "g": 255,
+        "b": 53
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 174,
+            "g": 255,
+            "b": 53
+          },
+          "TagText": "BALLER"
+        }
+      ]
+    },
+    "e0ff711d19912266a468bddc53a186a2b529f399021dfc3c90d68dafc759af11717bf6910dff7da6f9aeda717253167e735f180bf6af76389991f64414889248": {
+      "NameColor": {
+        "r": 255,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 255,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "VAPE USER"
+        }
+      ]
+    },
+    "b9ed6fcb2b83b27d55700500e1743cb907a6d4c8f59f4dac1be05288553830f7777c8441813247ae8e14aaca1a43765a942f240c48bd0efe3f9f1aa8dbbd08fe": {
+      "NameColor": {
+        "r": 255,
+        "g": 150,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 255,
+            "g": 150,
+            "b": 0
+          },
+          "TagText": "CONSTRUCTION MANAGER"
+        }
+      ]
+    },
+    "6acd9c2b023889762c27ed9ddab1b4533bc7c2674861fda5c1e2162f1c12c55621e627378ec331f515468886612fc558d12548414cc68698dfae2af0886db030": {
+      "NameColor": {
+        "r": 255,
+        "g": 150,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 255,
+            "g": 150,
+            "b": 0
+          },
+          "TagText": "CONSTRUCTION WORKER"
+        }
+      ]
+    },
+    "c067e91dec67df9b95b9ebac22b6a5fef5b6d790b80a41430a9f5ccf3e6a233cacae03eafcad0e5d57fb85a2b083cbfc7cc1c073a4c87b03f53ac206b5bd2ec3": {
+      "NameColor": {
+        "r": 0,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 0,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "TYPEWRITER"
+        }
+      ]
+    }
+  },
+  "specialusers": {
+    "529155884643909643": {
+      "NameColor": {
+        "r": 70,
+        "g": 70,
+        "b": 255
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 70,
+          "g": 70,
+          "b": 255
+        },
+        "TagText": "SYNAPSE OWNER"
+      }
+    },
+    "582558971722727424": {
+      "NameColor": {
+        "r": 40,
+        "g": 40,
+        "b": 140
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 40,
+          "g": 40,
+          "b": 140
+        },
+        "TagText": "SYNAPSE STAFF"
+      }
+    },
+    "1030586129466003537": {
+      "NameColor": {
+        "r": 70,
+        "g": 70,
+        "b": 255
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 70,
+          "g": 70,
+          "b": 255
+        },
+        "TagText": "SYNAPSE OWNER"
+      }
+    },
+    "384718982625886208": {
+      "NameColor": {
+        "r": 3,
+        "g": 207,
+        "b": 252
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 3,
+          "g": 207,
+          "b": 252
+        },
+        "TagText": "PRISM OWNER"
+      }
+    },
+    "548146409065545730": {
+      "NameColor": {
+        "r": 0,
+        "g": 255,
+        "b": 0
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 0,
+          "g": 255,
+          "b": 0
+        },
+        "TagText": "TYPEWRITER"
+      }
+    },
+    "837815287897128990": {
+      "NameColor": {
+        "r": 255,
+        "g": 255,
+        "b": 0
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 255,
+          "g": 255,
+          "b": 0
+        },
+        "TagText": "VAPE USER"
+      }
+    },
+    "709094619161296939": {
+      "NameColor": {
+        "r": 174,
+        "g": 255,
+        "b": 53
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 174,
+          "g": 255,
+          "b": 53
+        },
+        "TagText": "BALLER"
+      }
+    }
+  }
+}
